4af1185eb5f5d132403249a8de3c1bb726753cf8
Abhipsa Priyadarshini Sahoo <abhipsa priyadarshini sahoo>
2025-10-29 23:35:12 +0530

    day-3

---
+++ day-3-statefile/main.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "name" { 
+    instance_type = var.type
+     ami = var.ami_id
+     tags = {
+       Name = "prod"
+     }
+
+}
+
+resource "aws_s3_bucket" "name" {
+    bucket = "agdhgfhjhkghgfxgfhgkljkhgfdzxfghjkhgfchjk"
+    
+  
+}
+
+
+
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+      Name = " my_VPC"
+    }
+  
+}
