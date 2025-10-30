07574a986f7e666d54413c948b5a62c41008faa5
Abhipsa Priyadarshini Sahoo <abhipsa priyadarshini sahoo>
2025-10-30 20:33:04 +0530

    day-4 update

--- day-4-state-locking/backend.tf
+++ day-4-state-locking/backend.tf
@@ -2,6 +2,9 @@ terraform {
   backend "s3" {
     bucket = "mybucket30thoctober2025"
     key    = "terraform.tfstate"
+    use_lockfile = true 
     region = "us-east-1"
+     dynamodb_table = "abhipsa"
+    encrypt = true
   }
 }
